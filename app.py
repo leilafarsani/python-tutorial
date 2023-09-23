7049fca1e1bd63664268f152a3fd2df5867c321a
leilafarsani <108967493+leilafarsani@users.noreply.github.com>
2023-09-23 23:38:40 +0100

    slice the string

--- app.py
+++ app.py
@@ -21,3 +21,8 @@ print(message[1])
 print(course_name[0])
 print(course_name[-1])
 print(course_name[11])
+
+print(course_name[0:3])
+print(course_name[0:])
+print(course_name[:3])
+print(course_name[:])
